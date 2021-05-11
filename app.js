10a8cddffa245a45459547c9d6856d10f24a70db
ArslanRama <arslanrama84@gmail.com>
2021-05-11 10:22:01 +0200

    ejs file has been created

--- app.js
+++ app.js
@@ -59,3 +59,35 @@ app.get('/', (req, res) => {
         }
     });
 });
+
+// Step 8 - the POST handler for processing the uploaded file
+ 
+app.post('/', upload.single('image'), (req, res, next) => {
+ 
+    const obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    imgModel.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            res.redirect('/');
+        }
+    });
+});
+
+// Step 9 - configure the server's port
+ 
+const port = process.env.PORT || '3000'
+app.listen(port, err => {
+    if (err)
+        throw err
+    console.log('Server listening on port', port)
+})
