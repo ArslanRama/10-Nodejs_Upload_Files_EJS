b006921ed39de67537ab4e779be16fcde85cf2ab
ArslanRama <arslanrama84@gmail.com>
2021-05-11 10:11:34 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+ 
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+ 
+const fs = require('fs');
+const path = require('path');
+require('dotenv/config');
